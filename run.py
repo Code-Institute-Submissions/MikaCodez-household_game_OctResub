76e76fbafe3e7c9c422e4352295eec47e60b2423
Malcolm MikaZuki <malcolm.wiredu@gmail.com>
2022-02-12 19:30:43 +0000

    Initial   Commit

--- run.py
+++ run.py
@@ -34,3 +34,41 @@ def get_games_data():
             break
 
     return games_data
+
+
+def submit_game():
+
+    """
+    Call email, username, and game_info functions one by one
+
+    This function handles the automated python email sent to user
+
+    The email is sent from household game to the user email given.
+
+    As it is an automatic email, it is the user's responsibility
+
+    to enter valid information.
+
+    The credits of raw-python email code is mentioned in README.md
+    """
+
+global USER_EMAIL
+global FULL_NAME
+global GAME_DATA
+global HOURSPLAYED_DATA
+global GENRE_DATA
+global CONSOLE_DATA
+global STARRATING_DATA
+USER_EMAIL = email()
+FULL_NAME = username()
+print(FULL_NAME)
+GANE_DATA = game_info()
+print(GAME_DATA)
+HOURSPLAYED_DATA = hours_played_data()
+print(HOURSPLAYED_DATA)
+CONSOLE = console_data()
+print(CONSOLE_DATA)
+GENRE_DATA = genre_data()
+print(GENRE_DATA)
+STARRATING_DATA = star_rating_data()
+print(STARRATING_DATA)
