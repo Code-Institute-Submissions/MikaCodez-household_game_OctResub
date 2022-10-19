acb0fcfb9d6303b54de58d850307796f8b98a9d8
Malcolm MikaZuki <malcolm.wiredu@gmail.com>
2022-10-19 15:06:34 +0000

    attaching creds file to the rest of the pository to allow for api connectivity, also making sure APIS are wired up correctly. Can now confirm connection has been established. Now printing corrected data from spreadsheet to console

--- run.py
+++ run.py
@@ -30,81 +30,85 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('household_game')
 
- 
-    
-def get_games_data():
-    """
-    Call email, username, and game_info functions one by one
-    This function handles the automated python email sent to user
- 
-    The email is sent from household game to the user email given.
- 
-    As it is an automatic email, it is the user's responsibility
- 
-    to enter valid information.
- 
-    The credits of raw-python email code is mentioned in README.md
-    """
-    print("Welcome to HouseHold Game!")
-    '''
-    Graphic below from https://www.asciiart.eu/video-games/sonic-the-hedgehog
-    '''
-    print(
-                '''          .,
-                .      _,'f----.._
-                |\ ,-'"/  |     ,'
-                |,_  ,--.      /
-                /,-. ,'`.     (_
-                f  o|  o|__     "`-.
-                ,-._.,--'_ `.   _.,-`
-                `"' ___.,'` j,-'
-                `-.__.,--'"
-            "Here you will enter a A record of the games that you played, so that you can
-            log and track your favourite games"
-            "This will be then printed in a spreadsheet for you and sent over to your email!"
-            "Please enter game data"
-            "data should be as requested, seperated by commas"
-            "i.e. Email, Game, Genre, Hours played, Star rating\n"
-            "Are you ready to start?!!\n"''')
+games = SHEET.worksheet('games')
 
-def submit_games():
+data = games.get_all_values()
 
-    data = input("Enter your data here:\n ")
-    print(f"The data provided is {data}")
-
-    update_games_worksheet(data)
-    print(type(data))
-    returned_data = get_games_data()
-
- 
-def update_games_worksheet(data):
-    """
-    Update the games worksheet, by adding new row with the 
-    data provided.
-    """
-def get_user_input(prompt):
-    return input(prompt)
-
-def print_out_to_console(value_to_be_printed):
-    print(value_to_be_printed)
-email = get_user_input("Input your Email address\n")
-game = get_user_input("Input Game Title\n")
-genre = get_user_input("Input Game genre\n")
-console = get_user_input("Input Game Console\n")
-hours_played = get_user_input("Input gameplay hours\n")
-star_rating = get_user_input("Input Star Rating\n")
-print("updating games worksheet...\n")
-games_worksheet = SHEET.worksheet("games")
-worksheet_headings = games.worksheet.row_values(1)
-
-games_worksheet.append_row([data])
 print(data)
+    
+# def get_games_data():
+#     """
+#     Call email, username, and game_info functions one by one
+#     This function handles the automated python email sent to user
+ 
+#     The email is sent from household game to the user email given.
+ 
+#     As it is an automatic email, it is the user's responsibility
+ 
+#     to enter valid information.
+ 
+#     The credits of raw-python email code is mentioned in README.md
+#     """
+#     print("Welcome to HouseHold Game!")
+#     '''
+#     Graphic below from https://www.asciiart.eu/video-games/sonic-the-hedgehog
+#     '''
+#     print(
+#                 '''          .,
+#                 .      _,'f----.._
+#                 |\ ,-'"/  |     ,'
+#                 |,_  ,--.      /
+#                 /,-. ,'`.     (_
+#                 f  o|  o|__     "`-.
+#                 ,-._.,--'_ `.   _.,-`
+#                 `"' ___.,'` j,-'
+#                 `-.__.,--'"
+#             "Here you will enter a A record of the games that you played, so that you can
+#             log and track your favourite games"
+#             "This will be then printed in a spreadsheet for you and sent over to your email!"
+#             "Please enter game data"
+#             "data should be as requested, seperated by commas"
+#             "i.e. Email, Game, Genre, Hours played, Star rating\n"
+#             "Are you ready to start?!!\n"''')
+
+# def submit_games():
+
+#     data = input("Enter your data here:\n ")
+#     print(f"The data provided is {data}")
+
+#     update_games_worksheet(data)
+#     print(type(data))
+#     returned_data = get_games_data()
+
+ 
+# def update_games_worksheet(data):
+#     """
+#     Update the games worksheet, by adding new row with the 
+#     data provided.
+#     """
+# def get_user_input(prompt):
+#     return input(prompt)
+
+# def print_out_to_console(value_to_be_printed):
+#     print(value_to_be_printed)
+# email = get_user_input("Input your Email address\n")
+# game = get_user_input("Input Game Title\n")
+# genre = get_user_input("Input Game genre\n")
+# console = get_user_input("Input Game Console\n")
+# hours_played = get_user_input("Input gameplay hours\n")
+# star_rating = get_user_input("Input Star Rating\n")
+# print("updating games worksheet...\n")
+# games_worksheet = SHEET.worksheet("games")
+# worksheet_headings = games.worksheet.row_values(1)
+
+# games_worksheet.append_row([data])
+# print(data)
 
 
-print("Games worksheet updated successfully.\n")
+# print("Games worksheet updated successfully.\n")
 
 
-data = get_games_data()
-games_data = (worksheet.update)
-get_games_data()
-update_games_worksheet(returned_data)
+# data = get_games_data()
+# games_data = (worksheet.update)
+# get_games_data()
+# update_games_worksheet(returned_data)
